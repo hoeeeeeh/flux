39aa3e1249316f3dc9fb99a00895481879515dfb
Hoeeeeeh <hoeeeeeh@naver.com>
2024-09-03 00:02:52 +0900

    refactor: Store EventEmitter -> EventTarget
    웹브라우저 환경에서는 EventEmitter 를 사용할 수 없어 EventTarget 으로 수정했습니다.

--- mini_flux/Store.ts
+++ mini_flux/Store.ts
@@ -1,45 +1,44 @@
-import EventEmitter from 'node:events';
-import { dispatcher } from './Dispatcher';
-import { type Action } from './Action';
+import { dispatcher } from './Dispatcher.js';
+import { type Action } from '../action/Action.js';
 
-export default abstract class Store<TState> extends EventEmitter {
-    private readonly dispatcher = dispatcher;
+export default abstract class Store<TState> extends EventTarget {
+  private readonly dispatcher = dispatcher;
 
-    protected state: TState;
+  protected state: TState;
 
-    protected constructor(state: TState) {
-        super();
-        this.state = state;
-        // this.setState(state);
-    }
+  protected constructor(state: TState) {
+    super();
+    this.state = state;
+    // this.setState(state);
+  }
 
-    setState(newState: Partial<TState>) {
-        console.log('setState', newState);
-        this.state = { ...this.state, ...newState };
-        this.emitChange();
-    }
+  setState(newState: Partial<TState>) {
+    console.log('setState', newState);
+    this.state = { ...this.state, ...newState };
+    this.emitChange();
+  }
 
-    getState() {
-        return this.state;
-    }
+  getState() {
+    return this.state;
+  }
 
-    dispatcherRegister() {
-        this.dispatcher.register((action: Action) => {
-            this.reduce(action);
-        });
-    }
+  dispatcherRegister() {
+    this.dispatcher.register((action: Action) => {
+      this.reduce(action);
+    });
+  }
 
-    registerEvent(callback: (action: Action) => void) {
-        this.addListener('change', callback);
-    }
+  registerEvent(callback: (action: Action) => void) {
+    this.addEventListener('change', callback);
+  }
 
-    emitChange() {
-        this.emit('change');
-    }
+  emitChange() {
+    this.dispatchEvent(new Event('change'));
+  }
 
-    // stateChanged() {}
+  // stateChanged() {}
 
-    abstract reduce(action: Action): void;
+  abstract reduce(action: Action): void;
 }
 
 // export default { Store };
